91bad975b6deaf662d614b96f161280b5d11c9b0
Kai Schlamp <kai.schlamp@gmail.com>
2023-03-23 10:43:07 +0000

    Setup tooling

---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest"
+import { doit } from "./index"
+
+describe("First case", () => {
+  it("should succeed", () => {
+    expect(doit()).toBe("foobar")
+  })
+})
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function doit() {
+  return "foobar"
+}
