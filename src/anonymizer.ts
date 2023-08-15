9fe1c381dc11fafcd6c625d3257270da28c716ed
lmdulz <luca.dulz@med.uni-heidelberg.de>
2023-08-15 16:21:44 +0200

    implement datetimeanonymizer in anonymizer

--- src/anonymizer.ts
+++ src/anonymizer.ts
@@ -1,5 +1,4 @@
 import { Randomizer } from './randomizer';
-import { DicomDict, DicomMessage, Tag, DicomMetaDictionary} from 'dcmjs';
 import * as dcmjs from 'dcmjs';
 import { UnwantedElementStripper } from './unwantedelements';
 import { PNAnonymizer } from './pnanonymizer';
@@ -7,6 +6,7 @@ import { FixedValueAnonymizer } from './fixedvalueanonymizer';
 import { UIAnonymizer } from './uianonymizer';
 import { AddressAnonymizer } from './addressanonymizer';
 import { IDAnonymizer } from './idanonymizer';
+import { DateTimeAnonymizer } from './datetimeanonymizer';
 
 export class Anonymizer {
   /**
@@ -17,23 +17,27 @@ export class Anonymizer {
     
   */
   private patientID: string;
-  // private data: any;
+  
   private id_prefix: string | undefined;
   private id_suffix: string | undefined;
   private seed: string | undefined;
 
-  private minimum_offset_hours: number = 62 * 24;
-  private maximum_offset_hours: number = 730 * 24;
+  
   randomizer: Randomizer;
   element_handlers: any[];
-
-  //constructor(data: any, patientID: string, id_prefix?: string | undefined, id_suffix?: string | undefined, seed?: string | undefined) {
+  private date_offset_hours: number
+  
   constructor( patientID: string, id_prefix?: string | undefined, id_suffix?: string | undefined, seed?: string | undefined) {
+    const minimum_offset_hours: number = 62 * 24;
+    const maximum_offset_hours: number = 730 * 24;
     this.patientID = patientID;
-    // this.id_prefix = id_prefix;
+    this.id_prefix = id_prefix;
     this.id_suffix = id_suffix;
     this.seed = seed;
     this.randomizer = new Randomizer(this.seed)
+    this.date_offset_hours = Number(-(
+      this.randomizer.toInt("date_offset") % (BigInt(maximum_offset_hours) - BigInt(minimum_offset_hours)) + BigInt(minimum_offset_hours)
+    ))
     //this.data = data;
     this.element_handlers = 
         [new UnwantedElementStripper(["00101081",//"BranchOfService",
@@ -65,44 +69,10 @@ export class Anonymizer {
         new UIAnonymizer(this.randomizer).anonymize,
         new PNAnonymizer(this.randomizer).anonymize,
         new AddressAnonymizer(this.randomizer).anonymize,
-        new FixedValueAnonymizer("00100020", this.patientID).anonymize
-        
+        new FixedValueAnonymizer("00100020", this.patientID).anonymize,
+        new DateTimeAnonymizer(this.date_offset_hours).anonymize
     ];
-    // this.element_handlers = [
-    //   (dataset: any, tag: string) => { 
-    //     [new UnwantedElementStripper(["00101081",//"BranchOfService",
-    //                                   "00102180",//"Occupation",
-    //                                   "00101090",//"MedicalRecordLocator",
-    //                                   "00101080",//"MilitaryRank",
-    //                                   "00100050",//"PatientInsurancePlanCodeSequence",
-    //                                   "00102201",//"PatientReligiousPreference",
-    //                                   "00102155",//"PatientTelecomInformation",
-    //                                   "00102154",//"PatientTelephoneNumbers",
-    //                                   "00101100",//"ReferencedPatientPhotoSequence",
-    //                                   "00102299",//"ResponsibleOrganization"
-    //                                   ]).anonymize(dataset, tag),
-    //     new IDAnonymizer(this.randomizer, undefined, undefined,
-    //                                   "AccessionNumber",
-    //                                   "OtherPatientIDs",
-    //                                   "FillerOrderNumberImagingServiceRequest",
-    //                                   //"FillerOrderNumberImagingServiceRequestRetired", Retired
-    //                                   //"FillerOrderNumberProcedure", Retired
-    //                                   "PatientID",
-    //                                   "PerformedProcedureStepID",
-    //                                   "PlacerOrderNumberImagingServiceRequest",
-    //                                   //"PlacerOrderNumberImagingServiceRequestRetired",Retired
-    //                                   // "PlacerOrderNumberProcedure", Retired 
-    //                                   "RequestedProcedureID",
-    //                                   "ScheduledProcedureStepID",
-    //                                   "StationName",
-    //                                   "StudyID").anonymize(dataset, tag),                              
-    //     new UIAnonymizer(this.randomizer).anonymize(dataset, tag),
-    //     new PNAnonymizer(this.randomizer).anonymize(dataset, tag),
-    //     new AddressAnonymizer(this.randomizer).anonymize(dataset, tag),
-    //     new FixedValueAnonymizer("00100020", this.patientID).anonymize(dataset, tag)
-    //     ]
-    //   }
-    // ];
+    
   } 
   
   anonymize(data: any){
@@ -144,7 +114,7 @@ export class Anonymizer {
   }
  
   
-  del_private_tags(dataset: any, data_tag: string): Boolean {
+  del_private_tags(dataset: any, data_tag: string): boolean {
     const currTag = dcmjs.data.Tag.fromString(data_tag);
       //if (currTag.isPrivateCreator()){
       if (currTag.group() % 2 === 1){
