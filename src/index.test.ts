94cb42868d05713851957161f14530d40f73e7fd
Kai Schlamp <kai.schlamp@gmail.com>
2023-09-08 11:08:00 +0000

    Add anonymizer test

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { doit } from "./index";
-
-describe("First case", () => {
-  it("should succeed", () => {
-    expect(doit()).toBe("foobar");
-  });
-});
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export function doit() {
-  return "foobar";
-}
