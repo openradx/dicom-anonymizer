c09cc97e5edabfa5f3a5563cf162462a4a085a95
lmdulz <luca.dulz@med.uni-heidelberg.de>
2023-09-28 13:31:25 +0000

    test databasis and instance diff test

---
+++ tests/test_anonymize_one_series_two_instances.test.ts
@@ -0,0 +1,131 @@
+// Import your Anonymizer class
+import { data } from "dcmjs";
+import { describe, expect, it, test } from "vitest";
+// Import your data_for_tests module
+import { Anonymizer } from "../src/anonymizer";
+// Replace with your testing library imports
+import { loadInstance } from "./data_for_tests";
+
+// Replace with the actual structure of your dataset
+
+class OneSeriesTwoInstances {
+  dataset1: data.DicomDict;
+  dataset2: data.DicomDict;
+
+  constructor() {
+    this.dataset1 = loadInstance(1, 1);
+    this.dataset2 = loadInstance(1, 2);
+
+    const anonymizer = new Anonymizer("");
+    anonymizer.anonymize(this.dataset1);
+    anonymizer.anonymize(this.dataset2);
+  }
+}
+
+describe("patient", () => {
+  it("should anonymize patient, study, and series attributes the same", () => {
+    const diffInstances = new OneSeriesTwoInstances();
+    const dataset1 = diffInstances.dataset1;
+    const dataset2 = diffInstances.dataset2;
+    const elementPaths: string[][] = [
+      // patient
+      ["dict", "00100020"], // PatientID
+      ["dict", "00101000"], // OtherPatientIDs
+      ["dict", "00101002", "00100020"], // OtherPatientIDsSequence[0].PatientID
+      ["dict", "00101001"], // OtherPatientNames
+      ["dict", "00101040"], // PatientAddress
+      ["dict", "00100030"], // PatientBirthDate
+      ["dict", "00101005"], // PatientBirthName
+      ["dict", "00100032"], // PatientBirthTime
+      ["dict", "00101060"], // PatientMotherBirthName
+      ["dict", "00100010"], // PatientName
+      ["dict", "00102297"], // ResponsiblePerson
+    ];
+
+    for (const elementPath of elementPaths) {
+      if (elementPath.length == 2) {
+        console.log(elementPath);
+        const value1 = dataset1[elementPath[0]][elementPath[1]].Value[0];
+        const value2 = dataset2[elementPath[0]][elementPath[1]].Value[0];
+        expect(value1).toBe(value2);
+      } else {
+        const value1 = dataset1[elementPath[0]][elementPath[1]].Value[0][elementPath[2]].Value[0];
+        const value2 = dataset2[elementPath[0]][elementPath[1]].Value[0][elementPath[2]].Value[0];
+        expect(value1).toEqual(value2);
+      }
+    }
+  });
+
+  it("should anonymize instance attributes differently", () => {
+    const diffInstances = new OneSeriesTwoInstances();
+    const dataset1 = diffInstances.dataset1;
+    const dataset2 = diffInstances.dataset2;
+    const elementPaths = [
+      // study
+      ["dict", "0020000D"], // StudyInstanceUID
+      ["dict", "00081060"], // NameOfPhysiciansReadingStudy
+      ["dict", "00080090"], // ReferringPhysicianName
+      ["dict", "00321032"], // RequestingPhysician
+      ["dict", "00080050"], // AccessionNumber
+      ["dict", "00080020"], // StudyDate
+      ["dict", "00200010"], // StudyID
+      ["dict", "00080030"], // StudyTime
+      // series
+      ["dict", "0020000E"], // SeriesInstanceUID
+      ["dict", "00081070"], // OperatorsName
+      ["dict", "00081050"], // PerformingPhysicianName
+      ["dict", "00400275", "00401001"], // RequestAttributesSequence[0].RequestedProcedureID
+      ["dict", "00400275", "00400009"], // RequestAttributesSequence[0].ScheduledProcedureStepID
+      ["dict", "00200052"], // FrameOfReferenceUID
+      ["dict", "00080081"], // InstitutionAddress
+      ["dict", "00080080"], // InstitutionName
+      ["dict", "00400253"], // PerformedProcedureStepID
+      ["dict", "00401001"], // RequestedProcedureID
+      ["dict", "00400009"], // ScheduledProcedureStepID
+      ["dict", "00080021"], // SeriesDate
+      ["dict", "00080031"], // SeriesTime
+      // instance
+      ["dict", "00080018"], //SOPInstanceUID
+      ["meta", "00020003"], //MediaStorageSOPInstanceUID
+      ["dict", "00080012"], //InstanceCreationDate
+      ["dict", "00080013"], //InstanceCreationTime
+    ];
+
+    for (const elementPath of elementPaths) {
+      if (elementPath.length == 2) {
+        const value1 = dataset1[elementPath[0]][elementPath[1]].Value[0];
+        const value2 = dataset2[elementPath[0]][elementPath[1]].Value[0];
+        expect(value1).not.toEqual(value2);
+      } else {
+        const value1 = dataset1[elementPath[0]][elementPath[1]].Value[0][elementPath[2]].Value[0];
+        const value2 = dataset2[elementPath[0]][elementPath[1]].Value[0][elementPath[2]].Value[0];
+        expect(value1).not.toEqual(value2);
+      }
+    }
+  });
+});
+
+// function testWalk(set1: data.DicomDict, set2: data.DicomDict, tags2Check){
+//     const tagList1 = Object.keys(set1.dict);
+//     const tagList2 = Object.keys(set2.dict);
+//     expect(tagList1).toEqual(tagList2)
+
+//     for (const tag of tagList1) {
+//         if (elementPaths.includes(tag)) {
+//             const element1 = set1.dict[tag];
+//             const element2 = set2.dict[tag];
+//             expect(element1).toBe(element2)
+
+//             // If the element is a sequence, recursively walk through its items
+//             if (element1.vr === "SQ" && element2.vr === "SQ") {
+//                 for (let i = 0; i < element1.Value.length; i++) {
+//                 const sequence1 = element1.Value;
+//                 const sequence2 = element2.Value;
+//                 for (const item of sequence) {
+//                     this.walk(item, handler);
+//                 }
+//                 }
+//             }
+//         }
+//     }
+// }
