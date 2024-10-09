a017793600be01e136322be41724bfb79f3fa043
LucaDulz <lucamdulz@gmail.com>
2024-10-09 16:57:00 +0000

    exclude some files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/*.ts"],
+  entry: ["src/*.ts", '!src/loader.ts'],
   format: ["cjs", "esm"],
   dts: true,
   sourcemap: true,
